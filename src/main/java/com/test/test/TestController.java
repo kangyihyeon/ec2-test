2b3cbef977283e31d62a587a59d55c6898d78a56
WITCH <witch@WITCHui-MacBookPro.local>
2022-09-07 15:02:39 +0900

    test simple server

---
+++ src/main/java/com/test/test/TestController.java
@@ -0,0 +1,13 @@
+package com.test.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home(){
+        return "hello world";
+    }
+}
