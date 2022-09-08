08a6d7ec7ff102679ca2167b15ef6d8793e63b45
WITCH <witch@WITCHui-MacBookPro.local>
2022-09-08 10:41:59 +0900

    feat(index): index page using thymeleaf

--- src/main/java/com/test/test/TestController.java
+++ src/main/java/com/test/test/TestController.java
@@ -1,13 +1,18 @@
 package com.test.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
-    @GetMapping("/")
-    public String home(){
-        return "hello world";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("index", "hello world !!!");
+        return "/index";
     }
 }
