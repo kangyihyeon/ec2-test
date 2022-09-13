0ec7a2fbd2752e7bd5d3052197ddfc197ba106fb
Yihyeon <yhkang@witchcompany.io>
2022-09-13 16:30:35 +0900

    fix: 인스턴스 구분을 위한 address

--- src/main/java/com/test/test/TestController.java
+++ src/main/java/com/test/test/TestController.java
@@ -2,17 +2,22 @@ package com.test.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 @Controller
 public class TestController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Model model) {
+    public String home(Model model) throws UnknownHostException {
+        String hostAddress = Inet4Address.getLocalHost().getHostAddress();
+        String lastAddress = hostAddress.split("\\.")[3];
+
         model.addAttribute("index", "hello world !!!");
+        model.addAttribute("hostAddress", lastAddress);
+
         return "index";
     }
 }
